fc0297ec3f20f13dec1d5afe4acccda0bc360770
y-fujii <y-fujii@localhost>
2011-02-26 20:48:47 +0900

    use variant

--- ast.hpp
+++ ast.hpp
@@ -10,215 +10,162 @@ namespace ast {
 
 
 struct Expr {
-	enum Tag {
-		tWord,
-		tSubst,
-		tVar,
-		tPair,
-		tNull,
-		tConcat
-	};
-	Tag const tag;
-
-	protected:
-		Expr( Tag t ): tag( t ) {}
-
-	private:
-		Expr();
 };
 
 struct LeftExpr {
-	enum Tag {
-		tVarFix,
-		tVarVar
-	};
-	Tag const tag;
-
-	protected:
-		LeftExpr( Tag t ): tag( t ) {}
-	
-	private:
-		LeftExpr();
 };
 
 struct Stmt {
-	enum Tag {
-		tIf,
-		tCommand,
-		tFun,
-		tFetch,
-		tLet,
-		tReturn,
-		tBreak,
-		tFor,
-		tWhile,
-		tNot,
-		tOr,
-		tAnd,
-		tBg,
-		tSequence,
-		tRedirFr,
-		tRedirTo,
-		tPipe,
-		tNone
-	};
-	Tag const tag;
-
-	protected:
-		Stmt( Tag t ): tag( t ) {}
-
-	private:
-		Stmt();
 };
 
-struct Word: Expr {
-	Word( MetaString const& w ): Expr( tWord ), word( w ) {}
+struct Word: Variant<Expr, 0> {
+	Word( MetaString const& w ): word( w ) {}
 	MetaString word;
 };
 
-struct Subst: Expr {
-	Subst( Stmt* b ): Expr( tSubst ), body( b ) {}
+struct Subst: Variant<Expr, 1> {
+	Subst( Stmt* b ): body( b ) {}
 	Stmt* body;
 };
 
-struct Var: Expr {
-	Var( string const& n ): Expr( tVar ), name( n ) {}
+struct Var: Variant<Expr, 2> {
+	Var( string const& n ): name( n ) {}
 	string name;
 };
 
-struct Pair: Expr {
-	Pair( Expr* l, Expr* r ): Expr( tPair ), lhs( l ), rhs( r ) {}
+struct Pair: Variant<Expr, 3> {
+	Pair( Expr* l, Expr* r ): lhs( l ), rhs( r ) {}
 	Expr* lhs;
 	Expr* rhs;
 };
 
-struct Null: Expr {
-	Null(): Expr( tNull ) {}
+struct Null: Variant<Expr, 4> {
+	Null() {}
 };
 
-struct Concat: Expr {
-	Concat( Expr* l, Expr* r ): Expr( tConcat ), lhs( l ), rhs( r ) {}
+struct Concat: Variant<Expr, 5> {
+	Concat( Expr* l, Expr* r ): lhs( l ), rhs( r ) {}
 	Expr* lhs;
 	Expr* rhs;
 };
 
-struct VarFix: LeftExpr {
+struct VarFix: Variant<LeftExpr, 0> {
 	template<class Iter>
-	VarFix( Iter bgn, Iter end ): LeftExpr( tVarFix ), var( bgn, end ) {}
+	VarFix( Iter bgn, Iter end ): var( bgn, end ) {}
 	deque<Expr*> var;
 };
 
-struct VarVar: LeftExpr {
+struct VarVar: Variant<LeftExpr, 1> {
 	template<class Iter>
 	VarVar( Iter bgnL, Iter endL, Var* vM, Iter bgnR, Iter endR ):
-		LeftExpr( tVarVar ), varL( bgnL, endL ), varM( vM ), varR( bgnR, endR )  {}
+		varL( bgnL, endL ), varM( vM ), varR( bgnR, endR )  {}
 	deque<Expr*> varL;
 	Var* varM;
 	deque<Expr*> varR;
 };
 
-struct If: Stmt {
-	If( Stmt* c, Stmt* t, Stmt* e ): Stmt( tIf ), cond( c ), then( t ), elze( e ) {}
+struct If: Variant<Stmt, 0> {
+	If( Stmt* c, Stmt* t, Stmt* e ): cond( c ), then( t ), elze( e ) {}
 	Stmt* cond;
 	Stmt* then;
 	Stmt* elze;
 };
 
-struct Command: Stmt {
-	Command( Expr* a ): Stmt( tCommand ), args( a ) {}
+struct Command: Variant<Stmt, 1> {
+	Command( Expr* a ): args( a ) {}
 	Expr* args;
 };
 
-struct Fun: Stmt {
-	Fun( string const& n, LeftExpr* a, Stmt* b ): Stmt( tFun ), name( n ), args( a ), body( b ) {}
+struct Fun: Variant<Stmt, 2> {
+	Fun( string const& n, LeftExpr* a, Stmt* b ): name( n ), args( a ), body( b ) {}
 	string name;
 	LeftExpr* args;
 	Stmt* body;
 };
 
-struct Fetch: Stmt {
-	Fetch( LeftExpr* l ): Stmt( tFetch ), lhs( l ) {}
+struct Fetch: Variant<Stmt, 3> {
+	Fetch( LeftExpr* l ): lhs( l ) {}
 	LeftExpr* lhs;
 };
 
-struct Let: Stmt {
-	Let( LeftExpr* l, Expr* r ): Stmt( tLet ), lhs( l ), rhs( r ) {}
+struct Let: Variant<Stmt, 4> {
+	Let( LeftExpr* l, Expr* r ): lhs( l ), rhs( r ) {}
 	LeftExpr* lhs;
 	Expr* rhs;
 };
 
-struct Return: Stmt {
-	Return( Expr* r ): Stmt( tReturn ), retv( r ) {}
+struct Return: Variant<Stmt, 5> {
+	Return( Expr* r ): retv( r ) {}
 	Expr* retv;
 };
 
-struct Break: Stmt {
-	Break( Expr* r ): Stmt( tBreak ), retv( r ) {}
+struct Break: Variant<Stmt, 6> {
+	Break( Expr* r ): retv( r ) {}
 	Expr* retv;
 };
 
-struct For: Stmt {
-	For( Expr* v, Stmt* b, Stmt* e ): Stmt( tFor ), vars( v ), body( b ), elze( e ) {}
+struct For: Variant<Stmt, 7> {
+	For( Expr* v, Stmt* b, Stmt* e ): vars( v ), body( b ), elze( e ) {}
 	Expr* vars;
 	Stmt* body;
 	Stmt* elze;
 };
 
-struct While: Stmt {
-	While( Stmt* c, Stmt* b, Stmt* e ): Stmt( tWhile ), cond( c ), body( b ), elze( e ) {}
+struct While: Variant<Stmt, 8> {
+	While( Stmt* c, Stmt* b, Stmt* e ): cond( c ), body( b ), elze( e ) {}
 	Stmt* cond;
 	Stmt* body;
 	Stmt* elze;
 };
 
-struct Not: Stmt {
-	Not( Stmt* b ): Stmt( tNot ), body( b ) {}
+struct Not: Variant<Stmt, 9> {
+	Not( Stmt* b ): body( b ) {}
 	Stmt* body;
 };
 
-struct Or: Stmt {
-	Or( Stmt* l, Stmt* r ): Stmt( tOr ), lhs( l ), rhs( r ) {}
+struct Or: Variant<Stmt, 10> {
+	Or( Stmt* l, Stmt* r ): lhs( l ), rhs( r ) {}
 	Stmt* lhs;
 	Stmt* rhs;
 };
 
-struct And: Stmt {
-	And( Stmt* l, Stmt* r ): Stmt( tAnd ), lhs( l ), rhs( r ) {}
+struct And: Variant<Stmt, 11> {
+	And( Stmt* l, Stmt* r ): lhs( l ), rhs( r ) {}
 	Stmt* lhs;
 	Stmt* rhs;
 };
 
-struct Bg: Stmt {
-	Bg( Stmt* b ): Stmt( tBg ), body( b ) {}
+struct Bg: Variant<Stmt, 12> {
+	Bg( Stmt* b ): body( b ) {}
 	Stmt* body;
 };
 
-struct Sequence: Stmt {
-	Sequence( Stmt* l, Stmt* r ): Stmt( tSequence ), lhs( l ), rhs( r ) {}
+struct Sequence: Variant<Stmt, 13> {
+	Sequence( Stmt* l, Stmt* r ): lhs( l ), rhs( r ) {}
 	Stmt* lhs;
 	Stmt* rhs;
 };
 
-struct RedirFr: Stmt {
-	RedirFr( Stmt* b, Expr* f ): Stmt( tRedirFr ), body( b ), file( f ) {}
-	Stmt* body;
-	Expr* file;
-};
-
-struct RedirTo: Stmt {
-	RedirTo( Stmt* b, Expr* f ): Stmt( tRedirTo ), body( b ), file( f ) {}
+struct RedirFr: Variant<Stmt, 14> {
+	RedirFr( Stmt* b, Expr* f ): body( b ), file( f ) {}
 	Stmt* body;
 	Expr* file;
 };
 
-struct Pipe: Stmt {
-	Pipe( Stmt* l, Stmt* r ): Stmt( tPipe ), lhs( l ), rhs( r ) {}
+struct RedirTo: Variant<Stmt, 15> {
+	RedirTo( Stmt* b, Expr* f ): body( b ), file( f ) {}
+	Stmt* body;
+	Expr* file;
+};
+
+struct Pipe: Variant<Stmt, 16> {
+	Pipe( Stmt* l, Stmt* r ): lhs( l ), rhs( r ) {}
 	Stmt* lhs;
 	Stmt* rhs;
 };
 
-struct None: Stmt {
-	None(): Stmt( tNone ) {}
+struct None: Variant<Stmt, 17> {
+	None() {}
 };
 
 
