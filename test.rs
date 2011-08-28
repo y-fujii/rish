7e86f8a5f39157d0c06ca23801e50bb4ea27a406
y-fujii <y-fujii@localhost>
2011-08-29 01:27:25 +0900

    add test

---
+++ test.rs
@@ -0,0 +1,62 @@
+
+fun range $n {
+	let $i = 0
+	while test $i -lt $n {
+		yield $i
+		let $i = (expr $i + 1)
+	}
+}
+
+fun enumerate {
+	let $i = 0 
+	while fetch $e {
+		yield $i $e
+		let $i = (expr $i + 1)
+	}
+}
+
+fun at $n ($list) {
+	yield $list | enumerate | while fetch $i $e {
+		if test $i -eq $n {
+			yield $e
+			return 0
+		}
+	}
+	return 1
+}
+
+fun testDefer {
+	defer echo
+	range 8 | while fetch $i {
+		echo -n $i
+		defer echo -n $i
+	}
+	echo
+}
+
+fun factorialLoop $n {
+	let $result = 1
+	range $n | while fetch $i {
+		let $result = (expr $result "*" "(" $i + 1 ")" )
+	}
+	yield $result
+}
+
+fun factorialRec $n {
+	if test $n -le 1 {
+		yield 1
+	}
+	else {
+		expr $n "*" (factorialRec (expr $n - 1))
+	}
+}
+
+fun runTest {
+	echo (yield a b c)^(range 4)
+	factorialRec 8
+	factorialLoop 8
+	testDefer
+	at 4 (range 16)
+}
+
+runTest
