2abcec6a7e9d8e6c33625a7afc66339b350e29e1
Yasuhiro Fujii <y-fujii@mimosa-pudica.net>
2016-01-16 20:15:45 +0900

    add forgotten pch.hpp. remove tokens.hpp.

---
+++ pch.hpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cerrno>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+
+
+using namespace std;
